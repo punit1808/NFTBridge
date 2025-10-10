e4704591ca5b561a2d86d9186cf71f4dcd380386
punit1808 <punityadav1808@gmail.com>
2025-10-10 15:06:00 +0530

    1 erc721a

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -1,42 +1,5 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity >=0.8.0;
-
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-
-// contract NFTCollection is ERC721, Ownable {
-//     uint256 public tokenCounter;
-//     mapping(uint256 => string) private _tokenURIs;
-//     mapping(uint256 => string) private _prompts;
-
-//     constructor() ERC721("NFTCollection", "NFTC") {
-//         tokenCounter = 0;
-//     }
-
-//     function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
-//         _tokenURIs[tokenId] = _tokenURI;
-//     }
-
-//     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-//         return _tokenURIs[tokenId];
-//     }
-
-//     function promptDescription(uint256 tokenId) public view returns (string memory) {
-//         return _prompts[tokenId];
-//     }
-
-//     function mintNFT(address recipient, string memory uri, string memory prompt) public onlyOwner {
-//         uint256 newItemId = tokenCounter;
-//         _mint(recipient, newItemId);
-//         _setTokenURI(newItemId, uri);
-//         _prompts[newItemId] = prompt;
-//         tokenCounter += 1;
-//     }
-// }
-
-
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity >=0.8.0;
 
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -46,7 +9,7 @@ contract NFTCollectionA is ERC721A, Ownable {
     mapping(uint256 => string) private _tokenURIs;
     mapping(uint256 => string) private _prompts;
 
-    constructor() ERC721A("NFTCollection", "NFTC") Ownable(msg.sender) {
+    constructor() ERC721A("NFTCollection", "NFTC"){
         tokenCounter = 0;
     }
 
